30b05263203e0c3d62d78a2f6614085a9534a0de
Alexey Mozyrskiy <alexeymozyrskiydev@gmail.com>
2021-10-11 16:39:35 +0300

    Header. Разметка и верстка Имя и принадлежность пользователя

--- client/src/components/common/Header/index.tsx
+++ client/src/components/common/Header/index.tsx
@@ -8,13 +8,14 @@ import messagesIcon from 'src/library/icons/header/messages.svg';
 import bellIcon from 'src/library/icons/header/bell.svg';
 
 import st from './index.module.scss';
+import UserNameAndAffiliation from './frames/UserNameAndAffiliation';
 
 const Header: FC = () => {
   return (
     <header className={st.header}>
       <Search />
 
-      <div className={st.header__separator}></div>
+      <span className={st.header__separator} />
 
       <div className={st.header__icon}>
         <Icon icon={messagesIcon} color='green' />
@@ -29,8 +30,7 @@ const Header: FC = () => {
       </div>
 
       <div className={st.header__user}>
-        <h3 className={st.header__user__name}>name</h3>
-        <h4 className={st.header__user__affiliation}>affiliation</h4>
+        <UserNameAndAffiliation />
       </div>
 
       <div className={st.header__dots}>header__dots</div>
