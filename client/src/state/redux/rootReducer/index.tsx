778fd874e237c56925a6d95b91223534466bb42b
Alexey Mozyrskiy <msd.report.app@gmail.com>
2021-10-14 13:52:06 +0300

    RootReducer. Create

---
+++ client/src/state/redux/rootReducer/index.tsx
@@ -0,0 +1,17 @@
+import { createStore, combineReducers /*, applyMiddleware -*/ } from 'redux';
+// import thunkMiddleWare from 'redux-thunk';
+import { compose } from 'redux';
+// import usersReducers from "../users/reducer";
+
+let rootReducer = combineReducers({
+  // users: usersReducers,
+});
+
+type TRootReducer = typeof rootReducer; // тут будет (state: GlobslState) => GlobalState, typeof после отработки опрелдеит такой тип: (state: GlobslState) => GlobalState для функции rootStore
+export type TRootReducerSate = ReturnType<TRootReducer>; // тут мы вытащили тип GlobslState (то есть то что функуия возвращает) из типа редюсера
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // это для использования extension'а redux dev tools в Google Chrome
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(/* applyMiddleware(thunkMiddleWare)*/));
+
+export default store;
