bc27fed2aee4ac400f6b812667389cb43475d90b
Alexey Mozyrskiy <msd.report.app@gmail.com>
2021-10-14 13:55:35 +0300

    RootReducer. Provider в index.txt. Инициализация стора в приложении

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,15 @@ import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
+import store from 'src/state/redux/rootReducer';
+
 import './library/styles/normalizeCSS/normalize.scss';
 
 ReactDOM.render(
   <HashRouter basename={process.env.PUBLIC_URL}>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </HashRouter>,
   document.getElementById('root')
 );
