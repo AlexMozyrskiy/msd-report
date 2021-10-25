91cd1393b24d4c0efaa1be3a224c8c78d3eedcfa
Alexey Mozyrskiy <msd.report.app@gmail.com>
2021-10-25 14:38:38 +0300

    Cookie. На странице about cookie добавлен функцционал согласия и отклонения использования куки, запись в локал стораг

--- client/src/pages/user/AboutCookie/index.tsx
+++ client/src/pages/user/AboutCookie/index.tsx
@@ -1,11 +1,27 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+
+import {
+  getIsCookieAccepted as getIsCookieAcceptedLocalStorage,
+  setIsCookieAccepted as setIsCookieAcceptedLocalStorage,
+} from 'src/library/helpers/localStorage';
+
+import Button from 'src/library/components/Button';
 
 import st from './index.module.scss';
 
 const AboutCookie: FC = () => {
+  const [isCookieAccepted, setIsCookieAccepted] = useState<boolean>(
+    getIsCookieAcceptedLocalStorage() === 'true' ? true : false
+  );
+
+  const toggleIsCookieAccepted = (): void => {
+    setIsCookieAccepted(!isCookieAccepted);
+    setIsCookieAcceptedLocalStorage(isCookieAccepted ? 'false' : 'true');
+  };
+
   return (
     <section className={st.aboutCookie}>
-      <h2>Файлы Cookie</h2>
+      <h2>Cookie</h2>
       <p>
         Ку́ки (англ. cookie, букв. — «печенье») — небольшой фрагмент данных, отправленный веб-сервером и хранимый на
         компьютере пользователя.
@@ -27,6 +43,28 @@ const AboutCookie: FC = () => {
         система определяет авторизованы Вы или нет. <br />
         Без cookie refreshToken использование функционала этого приложения в полном объеме невозможно.
       </p>
+      <br />
+      <br />
+
+      {isCookieAccepted ? (
+        <>
+          <p className={st.cookieAccepted}>Вы согласились на использование Cookie</p>
+          <Button
+            text='Отменить использование Cookie этим сайтом'
+            onCkickHandler={toggleIsCookieAccepted}
+            width='long'
+          />
+        </>
+      ) : (
+        <>
+          <p className={st.cookieRejected}>Вы не согласились на использование Cookie</p>
+          <Button
+            text='Согласиться на использование Cookie этим сайтом'
+            onCkickHandler={toggleIsCookieAccepted}
+            width='long'
+          />
+        </>
+      )}
     </section>
   );
 };
