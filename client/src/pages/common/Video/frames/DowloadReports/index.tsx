baf8c1ce0f9e0594fbc570da364053970ff9a544
Alexey Mozyrskiy <msd.report.app@gmail.com>
2021-11-01 20:51:15 +0300

    > figure {

--- client/src/pages/common/Video/frames/DowloadReports/index.tsx
+++ client/src/pages/common/Video/frames/DowloadReports/index.tsx
@@ -8,17 +8,21 @@ import {
 } from 'src/state/redux/features/video/selectors';
 
 import ErrorMessage from 'src/library/components/ErrorMessage';
+import Button from 'src/library/components/Button';
+
+import telegramPicture from 'src/library/images/common/telegram.png';
 
 import st from './index.module.scss';
 
 const DownloadReports: FC = () => {
   const retreats = useSelector(getRetreatsSelector);
   const fileValidationErrors = useSelector(getFileValidationErrorSelector);
+
   if (!retreats.length && !fileValidationErrors.length) {
     return <ErrorMessage text='Данные не загружены, чтобы загрузить данные нажмите кнопку "Загрузить файл-шаблон"' />;
   } else if (!!fileValidationErrors.length) {
     return (
-      <article className={st.validationErros}>
+      <article className={st.reports}>
         <div className={st.wrapper}>
           <h2>В процессе загрузки файла возникли ошибки:</h2>
           <div className={st.info}>
@@ -32,7 +36,19 @@ const DownloadReports: FC = () => {
       </article>
     );
   } else {
-    return <article className={st.retreats}>Retreats Download</article>;
+    return (
+      <article className={st.reports}>
+        <div className={st.wrapper}>
+          <h2>Доступные отчеты</h2>
+
+          <div className={st.reports__item}>
+            <figure>
+              <img src={telegramPicture} alt='info' />
+            </figure>
+          </div>
+        </div>
+      </article>
+    );
   }
 };
 
